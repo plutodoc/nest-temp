1b006ede62a1bbb0873a1bd0f7c18b61b5ae8d2f
impossible98 <christoleavenow@outlook.com>
2023-12-05 22:43:27 +0800

    feat: init project

--- rspress.config.ts
+++ rspress.config.ts
@@ -60,6 +60,10 @@ export default defineConfig({
               text: 'Exception filters',
               link: '/overview/exception-filters',
             },
+            {
+              text: 'Pipes',
+              link: '/overview/pipes',
+            },
           ],
         },
       ],
